39582517847e3ca1d9078c28dd789373aed789fe
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-04-04 15:02:09 -0300

    convert to lib

--- src/main.zig
+++ src/main.zig
@@ -1,43 +1,11 @@
+const lib = @import("envfiles_lib");
 const std = @import("std");
-const Location = enum { InKey, InVal };
-const SyntaxError = error{ MalformedKey, MalformedVal };
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
-    var storage = std.StringHashMap([]const u8).init(arena.allocator());
-    const file = try std.fs.cwd().openFile(".env", .{});
-    const text = try file.readToEndAlloc(arena.allocator(), 1000);
-    var loc = Location.InKey;
-    var key_temp = std.ArrayList(u8).init(arena.allocator());
-    var val_temp = std.ArrayList(u8).init(arena.allocator());
-    for (text, 0..) |char, n| {
-        if (char == '=') {
-            if (loc == .InVal) {
-                return SyntaxError.MalformedKey;
-            }
-            loc = Location.InVal;
-            continue;
-        }
-        if ((char == '\n') or (n == text.len)) {
-            if (loc == .InKey) {
-                return SyntaxError.MalformedVal;
-            }
-            const key = try key_temp.toOwnedSlice();
-            const val = try val_temp.toOwnedSlice();
-            try storage.put(key, val);
-            loc = .InKey;
-            continue;
-        }
-        switch (loc) {
-            .InKey => try key_temp.append(char),
-            .InVal => try val_temp.append(char),
-        }
-    }
-    var map_iterator = storage.iterator();
-    while (map_iterator.next()) |entry| {
-        std.debug.print("key = {s}, value = {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-        arena.allocator().free(entry.key_ptr);
-        arena.allocator().free(entry.value_ptr);
-    }
+    const env = try lib.Env.init(".env", arena.allocator());
+    const v1 = try env.getVal("KEY");
+    std.debug.print("KF = {s}\n", .{v1});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,52 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
-const testing = std.testing;
-
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const Location = enum { InKey, InVal };
+const EnvFileSyntaxError = error{ MalformedKey, MalformedVal };
+const EnvFileError = error{NonExistantKey};
+fn getEnv(filepath: []const u8, allocator: std.mem.Allocator) !std.StringHashMap([]const u8) {
+    var storage = std.StringHashMap([]const u8).init(allocator);
+    const file = try std.fs.cwd().openFile(filepath, .{});
+    const text = try file.readToEndAlloc(allocator, 1000);
+    var loc = Location.InKey;
+    var key_temp = std.ArrayList(u8).init(allocator);
+    var val_temp = std.ArrayList(u8).init(allocator);
+    for (text, 0..) |char, n| {
+        if (char == '=') {
+            if (loc == .InVal) {
+                return EnvFileSyntaxError.MalformedKey;
+            }
+            loc = Location.InVal;
+            continue;
+        }
+        if ((char == '\n') or (n == text.len)) {
+            if (loc == .InKey) {
+                return EnvFileSyntaxError.MalformedVal;
+            }
+            const key = try key_temp.toOwnedSlice();
+            const val = try val_temp.toOwnedSlice();
+            try storage.put(key, val);
+            loc = .InKey;
+            continue;
+        }
+        switch (loc) {
+            .InKey => try key_temp.append(char),
+            .InVal => try val_temp.append(char),
+        }
+    }
+    return storage;
 }
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const Env = struct {
+    allocator: std.mem.Allocator,
+    envs: std.StringHashMap([]const u8),
+    pub fn init(filepath: []const u8, allocator: std.mem.Allocator) !Env {
+        const env = try getEnv(filepath, allocator);
+        return Env{ .allocator = allocator, .envs = env };
+    }
+    pub fn getVal(self: Env, key: []const u8) EnvFileError![]const u8 {
+        const valquery = self.envs.get(key);
+        if (valquery) |val| {
+            return val;
+        } else {
+            return EnvFileError.NonExistantKey;
+        }
+    }
+};
